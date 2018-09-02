0fa73bd272d7e18bff18917e7c7597cd62fe150e
maiyan <yan.mai@xfxb.net>
2018-09-02 11:24:53 +0800

    ESLint+Prettier

--- example/src/index.js
+++ example/src/index.js
@@ -11,7 +11,7 @@ const app = dva();
 // app.model(require('./models/example').default);
 
 // 4. Router
-app.router(require('./router').default);
+app.router(require('./router'));
 
 // 5. Start
 app.start('#root');
